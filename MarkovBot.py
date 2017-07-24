e0f682a9b0522819d2ac76a3f901b744d9376453
mvanamburg <MatthewTvanamburg@gmail.com>
2017-07-24 15:31:34 -0400

    Add files via upload

---
+++ MarkovBot.py
@@ -0,0 +1,55 @@
+import os
+import discord
+import asyncio
+import markovify
+
+client = discord.Client()
+
+def mchain(parse):
+    text_model = markovify.NewlineText(parse)
+    return(str(text_model.make_short_sentence(140)))
+
+@client.event
+async def on_ready():
+    print('Logged in as'+ ' '+ client.user.name)
+@client.event
+async def on_message(message):
+	if message.content.startswith('!markov') and message.author != client.user:
+	
+		markov_target = message.content[len('!markov'):].strip()
+		print (markov_target)
+		
+		updating_archive = open('updating_archive.log', 'w')
+		
+		async for message in client.logs_from(message.channel, limit=500):
+			updating_archive.write(str(message.author)[:-5]+' '+str(message.content)+'\n')
+			
+		updating_archive.close()
+		
+		markov_target_parsed = open(markov_target+'.log', 'w')
+		
+		number_of_lines = 0
+		updating_archive = open('updating_archive.log', 'r')
+		for line in updating_archive:
+			if line.startswith(markov_target):
+				markov_target_parsed.write(str(line[len(markov_target):].strip()+'\n'))
+				number_of_lines += 1
+				
+		markov_target_parsed.close()
+		updating_archive.close()
+		
+		print (str(number_of_lines) + ' lines containing '+(str(markov_target)))
+		if number_of_lines == 0:
+			await client.send_message(message.channel, ("This person does not exist in my archives. Remember that names are case sensitive."))
+			os.remove(markov_target+'.log')
+			print ("Useless log file deleted")
+		else:
+			markov_output = open(markov_target+'.log', 'r').read()
+			await client.send_message(message.channel, (mchain(markov_output)))
+			print ("Generation successful!")
+
+#Definitely didnt steal this part			
+if __name__ == '__main__':
+    with open("creds") as f:
+        creds = f.readlines()[0].strip()
+client.run(creds)
