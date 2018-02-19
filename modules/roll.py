3d650a3420f0306ed3f7968ca6cbad03d5847dbd
Matthew Vanamburg <MatthewTvanamburg@gmail.com>
2018-02-19 02:31:07 -0500

    Update roll.py

--- modules/roll.py
+++ modules/roll.py
@@ -1,11 +1,12 @@
 import random
 
+
 def roll_die(roll_min, roll_max):
-    #print (str(roll_min) + str(roll_max))
+    # print (str(roll_min) + str(roll_max))
     roll_min = int(roll_min)
     roll_max = int(roll_max)
-    if roll_min <=0 or roll_max <=0 or roll_min > 9999999999 or roll_max > 9999999999 or roll_min > roll_max:
+    if roll_min <= 0 or roll_max <= 0 or roll_min > 9999999999 or roll_max > 9999999999 or roll_min > roll_max:
         return ("Arguments must not be less than 0 or greater than 9999999999 and the minimum roll can not be greater than the maximum roll.")
     else:
-        random_roll = (random.randint(roll_min,roll_max))
+        random_roll = (random.randint(roll_min, roll_max))
         return (" rolled a "+str(random_roll)+".")
